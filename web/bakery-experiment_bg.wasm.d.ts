4d4801f304dc019528715587a0a72580e6918fc4
Luke Sontrop <lukerobertsontrop@gmail.com>
2026-06-18 23:58:02 -0700

    Adds built wasm files.

---
+++ web/bakery-experiment_bg.wasm.d.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: () => void;
+export const wgpu_render_bundle_draw: (a: number, b: number, c: number, d: number, e: number) => void;
+export const wgpu_render_bundle_draw_indexed: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+export const wgpu_render_bundle_set_pipeline: (a: number, b: bigint) => void;
+export const wgpu_render_bundle_draw_indirect: (a: number, b: bigint, c: bigint) => void;
+export const wgpu_render_bundle_set_bind_group: (a: number, b: number, c: bigint, d: number, e: number) => void;
+export const wgpu_render_bundle_set_vertex_buffer: (a: number, b: number, c: bigint, d: bigint, e: bigint) => void;
+export const wgpu_render_bundle_set_push_constants: (a: number, b: number, c: number, d: number, e: number) => void;
+export const wgpu_render_bundle_draw_indexed_indirect: (a: number, b: bigint, c: bigint) => void;
+export const wgpu_render_bundle_insert_debug_marker: (a: number, b: number) => void;
+export const wgpu_render_bundle_pop_debug_group: (a: number) => void;
+export const wgpu_render_bundle_set_index_buffer: (a: number, b: bigint, c: number, d: bigint, e: bigint) => void;
+export const wgpu_render_bundle_push_debug_group: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h880e94e8c7d94572: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h4e0f57ce8b80fd3e: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0a5ef6215ff6efc2: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0a5ef6215ff6efc2_3: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0a5ef6215ff6efc2_4: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0a5ef6215ff6efc2_5: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0a5ef6215ff6efc2_6: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0a5ef6215ff6efc2_7: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0a5ef6215ff6efc2_8: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0a5ef6215ff6efc2_9: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__hc97aabd238134451: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__hdd68028301c8d486: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
